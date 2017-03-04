41634150ed7a108a27e33a21ac061450b18b9653
links234 <murtaza_alexandru73@yahoo.com>
2017-03-04 12:05:07 -0800

    Fix install.sh to be pwd independent

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-python3 install.py install
-python3 install.py mock
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
+python3 install.py $MY_PATH/install
+python3 install.py $MY_PATH/mock
