2e4f3281e0303cbb900c377096c191fd5166480b
Mihai Ciucu <mihai@infra.org>
2022-09-27 17:36:38 +0300

    Looked like garbage

--- establishment-nodews-proxy/source/EntryPoint.js6.js
+++
@@ -1,4 +0,0 @@
-module.exports.DefaultConfig = require("./DefaultConfig.js6.js");
-module.exports.ProxiedWebsocketConnection = require("./ProxiedWebsocketConnection.js6.js");
-module.exports.Server = require("./Server.js6.js");
-module.exports.WebsocketProxyServer = require("./WebsocketProxyServer.js6.js");
